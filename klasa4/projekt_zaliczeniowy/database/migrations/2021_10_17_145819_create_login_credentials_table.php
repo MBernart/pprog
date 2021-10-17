2e06dd3091e0d0d32664905955c6479146848d09
MBernart <mateuszbernart@gmail.com>
2021-10-17 18:16:54 +0200

    first part of db structure

--- klasa4/projekt_zaliczeniowy/database/migrations/2021_10_17_145819_create_login_credentials_table.php
+++ klasa4/projekt_zaliczeniowy/database/migrations/2021_10_17_145819_create_login_credentials_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateLoginCredentialsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('login_credentials', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->foreignIdFor(\App\Models\LoginCredentials::class, "related_login_id");
-            $table->rememberToken();
+            $table->string('login');
+            $table->string('password');
+            $table->string('salt');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('login_credentials');
     }
 }
--- klasa4/projekt_zaliczeniowy/database/migrations/2021_10_17_154655_create_courses_table.php
+++ klasa4/projekt_zaliczeniowy/database/migrations/2021_10_17_154655_create_courses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCoursesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->foreignIdFor(\App\Models\LoginCredentials::class, "related_login_id");
-            $table->rememberToken();
+            $table->string('name');
+            $table->text('description');
+            $table->foreignIdFor(\App\Models\User::class, "owner_id");
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('courses');
     }
 }
