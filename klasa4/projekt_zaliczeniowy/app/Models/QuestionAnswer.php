91b233474e46c726150b0a8bcbcec60414936460
MBernart <mateuszbernart@gmail.com>
2021-10-28 21:59:10 +0200

    Get TestQuestion for QuestionAnswer

--- klasa4/projekt_zaliczeniowy/app/Models/QuestionAnswer.php
+++ klasa4/projekt_zaliczeniowy/app/Models/QuestionAnswer.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class QuestionAnswer extends Model
 {
     use HasFactory;
+
+    public function Question()
+    {
+        return $this->belongsTo(TestQuestion::class, 'question_id', 'id');
+    }
 }
