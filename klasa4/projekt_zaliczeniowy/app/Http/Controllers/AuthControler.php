3ea8733a3d84b2aac512289036290475f0f3fbc3
MBernart <mateuszbernart@gmail.com>
2022-01-01 21:59:53 +0100

    Added Session::flush() on logout

--- klasa4/projekt_zaliczeniowy/app/Http/Controllers/AuthControler.php
+++ klasa4/projekt_zaliczeniowy/app/Http/Controllers/AuthControler.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
@@ -33,6 +34,9 @@ class AuthController extends Controller
     public function logout(Request $request)
     {
         Auth::logout();
-        return redirect()->guest('login');
+
+        Session::flush();
+
+        return redirect('login');
     }
 }
