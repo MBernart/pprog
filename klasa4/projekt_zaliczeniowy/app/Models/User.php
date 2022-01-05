48c9d4a57ada2540443f8cf824438823ef0faabf
MBernart <mateuszbernart@gmail.com>
2022-01-05 22:04:47 +0100

    user password hashing

--- klasa4/projekt_zaliczeniowy/app/Models/User.php
+++ klasa4/projekt_zaliczeniowy/app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -20,7 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'username',
         'email',
-        'related_login'
+        'password'
     ];
 
     /**
@@ -51,5 +52,10 @@ class User extends Authenticatable
     public function username()
     {
         return 'username';
+    }
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = Hash::make($password);
     }
 }
