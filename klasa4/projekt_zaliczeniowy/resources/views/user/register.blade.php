0ec6cfe9ced2d21dc6ebd2147526b3fc4433fb67
MBernart <mateuszbernart@gmail.com>
2022-03-06 18:49:13 +0100

    🐛register doesn't redirect to updateEmail anymore

--- klasa4/projekt_zaliczeniowy/resources/views/user/register.blade.php
+++ klasa4/projekt_zaliczeniowy/resources/views/user/register.blade.php
@@ -10,7 +10,7 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('updateEmail') }}">
+                    <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="row mb-3">
