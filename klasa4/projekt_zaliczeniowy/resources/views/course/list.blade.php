010e162c1079e6e32e9e6535686f5ec27d1fcb19
MBernart <mateuszbernart@gmail.com>
2022-03-06 19:28:41 +0100

    :sparkles: translations

--- klasa4/projekt_zaliczeniowy/resources/views/course/list.blade.php
+++ klasa4/projekt_zaliczeniowy/resources/views/course/list.blade.php
@@ -8,18 +8,18 @@
 
     <div class="row justify-content-center d-flex align-items-center">
         <!-- <div class="col-md-10"> -->
-        <div class="col text-center"><strong>Nazwa</strong></div>
-        <div class="col-3 text-center"><strong>Opis</strong></div>
-        <div class="col text-center"><strong>Właściciel</strong></div>
-        <div class="col text-center"><strong>Data ostatniej modyfikacji</strong></div>
-        <div class="col text-center"><strong>Liczba uczestników</strong></div>
+        <div class="col text-center"><strong>{{ __('Nazwa') }}</strong></div>
+        <div class="col-3 text-center"><strong>{{ __('Opis') }}</strong></div>
+        <div class="col text-center"><strong>{{ __('Właściciel') }}</strong></div>
+        <div class="col text-center"><strong>{{ __('Data ostatniej modyfikacji') }}</strong></div>
+        <div class="col text-center"><strong>{{ __('Liczba uczestników') }}</strong></div>
     </div>
     <hr>
 
     @foreach (Auth::user()->CoursesTheMemberIsAssignedTo as $course)
     <div class="row justify-content-center d-flex align-items-center">
 
-        <div class="col text-center"><a class="text-decoration-none fw-bold" href="{{ url('course', $course->id) }}?tab=about">{{ $course->name }} </a> </div>
+        <div class="col text-center"><a class="text-decoration-none fw-bold" href="{{ url('course', $course->id) }}?tab=tests">{{ $course->name }} </a> </div>
         <div class="col-3 text-center">{{ $course->description }}</div>
         <div class="col text-center">{{ $course->Owner->username }}</div>
         <div class="col text-center">{{ $course->updated_at }}</div>
@@ -32,7 +32,7 @@
     @endforeach
     <div class="row justify-content-center d-flex align-items-center mt-3">
         <div class="col-6 text-center border-top pt-2">
-            <span class="h5">Nie ma więcej kursów do wyświetlenia</span>
+            <span class="h5">{{ __('Nie ma więcej kursów do wyświetlenia') }}</span>
         </div>
     </div>
 </div>
